4a5a3bcaaa9d3dc645d74ef5a2822d2408da58f5
Osamudiame Ruth Igbinoba <105964264+bluesense-core@users.noreply.github.com>
2022-11-18 11:11:20 +0100

    updates

---
+++ src/components/Cards.js
@@ -0,0 +1,37 @@
+import alarm from '../images/operation-img/alarm.png';
+import wckh from '../images/operation-img/wckh.png';
+import bas from '../images/operation-img/bas.png';
+import fss from '../images/operation-img/fss.png';
+
+const OpCards = [
+    {
+        id: 1,
+        title: 'Fire Alarm/Detection Systems',
+        subtitle:
+            'Chrisason ensures the fire safety through the installation and maintenance of fire alarm systems. Special care is taken by our team in selecting one that is well suited to your company needs.',
+        img: alarm,
+    },
+    {
+        id: 2,
+        title: 'Wet Chemical Kitchen Hood Fire Suppression System',
+        subtitle:
+            'We specialise in the design, supply, install, service, maintain, recharge of wet chemical hood fire suppression systems to enhance fire protection when spills or accidents occur in the kitchen.',
+        img: wckh,
+    },
+    {
+        id: 3,
+        title: 'Breathing Air Systems',
+        subtitle:
+            'We also specialise in installing Breathing Air Systems which are vital in fire safety. Breathing systems are also important for those who live in high-rise buildings. These systems can help firefighters reach areas where there may be higher concentrations of smokes.',
+        img: bas,
+    },
+    {
+        id: 4,
+        title: 'FM200, CO2, Inergen and Argon Suppression Systems',
+        subtitle:
+            'We specialise in installing and maintaining various fire suppression systems including FM200, Inergen, Argon and CO2. They are all essential to the fire protection field and Chrisason Fire plays a vital part in ensuring their usefulness.',
+        img: fss,
+    },
+];
+
+export { OpCards };
--- src/ui/Operations.js
+++ src/ui/Operations.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import './operations.css';
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Col, Container, Image, Row, Card } from 'react-bootstrap';
 import op1 from '../images/operation-img/op1.png';
 import op2 from '../images/operation-img/op2.png';
 import op3 from '../images/operation-img/op3.png';
 import op4 from '../images/operation-img/op4.png';
 import aside from '../images/operation-img/aside.png';
-import FM200 from '../images/operation-img/FM200.svg';
-import CO2 from '../images/operation-img/CO2.svg';
-import inergen from '../images/operation-img/inergen.svg';
-import argon from '../images/operation-img/argon.svg';
-import wckh from '../images/operation-img/wckh.svg';
-import alarm from '../images/operation-img/alarm.svg';
+import { OpCards } from '../components/Cards';
 
 export default function Operations() {
     return (
@@ -26,8 +21,8 @@ export default function Operations() {
 
             <section className='recent-operations text-center py-5'>
                 <Container>
-                    <h2 className='title mb-2'>
-                        FIRE SUPPRESSION SYSTEMS (FSS)
+                    <h2 className='title mb-2 text-uppercase'>
+                        Fire Protection Operations
                     </h2>
                     <p className='subtitle px-5'>
                         Chrisason Fire takes great care in ensuring that fire
@@ -35,124 +30,27 @@ export default function Operations() {
                         the services used to achieve proper fire safety are fire
                         suppression systems which come in different variations
                     </p>
-                    <Row className='justify-content-md-center align-items-center py-5 g-md-5 gy-5'>
-                        <Col className='h-100' md={6} lg={4}>
-                            <Image
-                                src={FM200}
-                                alt='FM200 Fire Suppression System'
-                            />
-                            <h3 className='my-3'>
-                                FM200 Fire Suppression System
-                            </h3>
-                            <p>
-                                We specialise in installing and maintaining
-                                FM200 which is the most used suppression in the
-                                industry at the minute and it is what we
-                                recommend for our clients in similar industries.
-                                It is a colourless, compressed liquefied gas
-                                used to extinguish fires. It is also pressurised
-                                with nitrogen, waterless, and upon activation,
-                                discharges as a gas to suppress the fire.
-                            </p>
-                        </Col>
 
-                        <Col className='h-100' md={6} lg={4}>
-                            <Image
-                                src={CO2}
-                                alt='CO2 Fire Suppression System'
-                            />
-                            <h3 className='my-3'>
-                                CO2 Fire Suppression System
-                            </h3>
-                            <p>
-                                A carbon dioxide (CO2) fire suppression system
-                                is a system that is designed to suppress the
-                                production of carbon dioxide (CO2) by a human or
-                                animal source. Carbon dioxide fire suppression
-                                systems can be used in both urban and forest
-                                fires.
-                            </p>
-                        </Col>
-
-                        <Col className='h-100' md={6} lg={4}>
-                            <Image
-                                src={inergen}
-                                alt='Inergen Fire Suppression System'
-                            />
-                            <h3 className='my-3'>
-                                Inergen Fire Suppression System
-                            </h3>
-                            <p>
-                                {' '}
-                                This automatically extinguishes fires when they
-                                start. This system can be used in all types of
-                                buildings, including homes and commercial
-                                buildings. Inergen also has a smoke detector
-                                that can be installed in the building to alert
-                                occupants if smoke is present in the building.
-                            </p>
-                        </Col>
-
-                        <Col md={6} lg={5}>
-                            <Image
-                                src={argon}
-                                alt='Argon Fire Suppression System'
-                            />
-                            <h3 className='my-3'>
-                                Argon Fire Suppression System
-                            </h3>
-                            <p>
-                                The main purpose of an Argon fire suppression
-                                system is to prevent fires from spreading too
-                                far and causing damage to property or buildings.
-                                By controlling the amount of air that passes
-                                through the system over time, you can keep fires
-                                under control without having to constantly move
-                                air tanks around.Its most common use is for
-                                homes located in remote areas where there are no
-                                nearby residents or buildings that may pose a
-                                threat to safety.
-                            </p>
-                        </Col>
-
-                        <Col md={6} lg={5}>
-                            <Image src={wckh} alt='' />
-                            <h3 className='my-3'>
-                                Wet Chemical Kitchen Hood Fire Suppression
-                                System
-                            </h3>
-                            <p>
-                                We specialise in the design, supply, install,
-                                service, maintain, recharge of wet chemical hood
-                                fire suppression systems to enhance fire
-                                protection when spills or accidents occur in the
-                                kitchen. It’s best for kitchens that have a high
-                                amount of carbon dioxide emissions, such as
-                                refrigerated food storage facilities.
-                            </p>
-                        </Col>
-                    </Row>
-
-                    <h2 className='title mb-3'>FIRE ALARM/DETECTION SYSTEMS</h2>
-
-                    <Row className='justify-content-md-center align-items-center py-5 g-md-5 gy-5'>
-                        <Col className='h-100' lg={6}>
-                            <Image
-                                src={alarm}
-                                alt='Fire Alarm/Detection Systems'
-                            />
-                            <h3 className='my-3'>
-                                Fire Alarm/Detection Systems
-                            </h3>
-                            <p>
-                                Chrisason ensures the fire safety through the
-                                installation and maintenance of fire alarm
-                                systems. Special care is taken by our team in
-                                selecting one that is well suited to your
-                                company needs and complies with British
-                                Standards.
-                            </p>
-                        </Col>
+                    <Row xs={1} md={2} className='pt-5 g-5  align-items-center'>
+                        {OpCards.map((e) => (
+                            <Col key={e.id}>
+                                <Card className='h-100 border-0 rounded-0 text-center'>
+                                    <Card.Img
+                                        variant='top'
+                                        src={e.img}
+                                        alt={e.title}
+                                    />
+                                    <Card.Body>
+                                        <Card.Title className='fs-4 my-2'>
+                                            {e.title}
+                                        </Card.Title>
+                                        <Card.Text className='px-5 px-md-0 px-xl-5 pt-2'>
+                                            {e.subtitle}
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        ))}
                     </Row>
                 </Container>
             </section>
