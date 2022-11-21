9ba4cbaaaabb1522167b0ad8b51f1daa66dce8d3
Osamudiame Ruth Igbinoba <105964264+bluesense-core@users.noreply.github.com>
2022-11-21 13:20:00 +0100

    update

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,6 +2,10 @@ import alarm from '../images/operation-img/alarm.png';
 import wckh from '../images/operation-img/wckh.png';
 import bas from '../images/operation-img/bas.png';
 import fss from '../images/operation-img/fss.png';
+import Project1 from '../images/operation-img/project-1.webp';
+import Project3 from '../images/operation-img/project-3.webp';
+import Project5 from '../images/operation-img/project-5.webp';
+import Project6 from '../images/operation-img/project-6.webp';
 
 const OpCards = [
     {
@@ -31,6 +35,39 @@ const OpCards = [
         subtitle:
             'We specialise in installing and maintaining various fire suppression systems including FM200, Inergen, Argon and CO2. They are all essential to the fire protection field and we ensure their usefulness.',
         img: fss,
+    },
+    {
+        id: 5,
+        img: Project5,
+        title: 'Tank Protection System',
+        subtitle:
+            'Fire in a storage tank, or in the surrounding bund, is a challenge to both fire fighters and tank operators. The value of the contents makes the provision...',
+        link: '/',
+    },
+    {
+        id: 6,
+        img: Project6,
+        title: 'Factories + Industrial Fire Protection',
+        subtitle:
+            'Factories and warehouses around the country and the world are responsible for the manufacture and storage of countless items that we use on a daily basis...',
+        link: '/',
+    },
+    {
+        id: 7,
+        img: Project1,
+        title: 'Fire Safety & QHSE Training',
+        subtitle:
+            'Our fire safety training courses offer comprehensive, accredited training to individuals, managers and organisations. We offer fire safety courses in....',
+        link: '/',
+    },
+
+    {
+        id: 8,
+        img: Project3,
+        title: 'Maintenance Service Levels',
+        subtitle:
+            'Our engineers are highly trained to assemble + install fire alarm + fire suppression systems. Alongside our installation, we will offer r inspection, testing + maintenance...',
+        link: '/',
     },
 ];
 
